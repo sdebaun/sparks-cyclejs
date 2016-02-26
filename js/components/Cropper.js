fade220ab9ab60cce08c4d7532796280ebdb4c94
TylorS <tlsteinberger167@gmail.com>
2016-02-26 16:00:17 -0500

    style(js): adjust source code to conform to the eslint config
    Adjusts the style of all the current code to match what is expected by
    the eslint config.

--- js/components/Cropper.js
+++ js/components/Cropper.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Cropper from 'react-cropper'
 import {BehaviorSubject} from 'rx'
 import {reactComponent} from 'helpers'
 
-// stupid react component requires ref (and thus a class) to get dataurl from canvas
+// stupid react component requires ref (and thus a class)
+// to get dataurl from canvas
 class ReactCropper extends React.Component {
-  crop = ()=>this.props.onCrop(this.refs.cropper.getCroppedCanvas().toDataURL())
+  crop = () => this.props.onCrop(
+    this.refs.cropper.getCroppedCanvas().toDataURL()
+  )
 
   render() {
-    return <Cropper ref='cropper' {...{...this.props,crop:this.crop,autoCrop:true}}/>
+    return <Cropper ref='cropper' {
+      ...{...this.props, crop: this.crop, autoCrop: true}
+    }/>
   }
 }
 
-export default ({image$}) =>{
+export default ({image$}) => {
   const cropped$ = new BehaviorSubject(null)
   return {
     cropped$,
-    DOM: image$ // has to be attached on 'update', the default, breaks if 'insert'
-      .map( src=>reactComponent(ReactCropper, {src,onCrop:e=>cropped$.onNext(e),aspectRatio:1},'update') )
+    // has to be attached on 'update', the default, breaks if 'insert'
+    DOM: image$
+      .map(src => reactComponent(ReactCropper, {
+        src,
+        onCrop: e => cropped$.onNext(e),
+        aspectRatio: 1,
+      }, 'update')),
   }
 }
