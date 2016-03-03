5c144811668b19acdb934c20accb84233990806c
Stevo <sdebaun@sparks.network>
2016-03-03 04:48:28 -0800

    crazy layout composition

--- src/root/Project/index.js
+++ src/root/Project/index.js
@@ -1,7 +1,7 @@
 import {Observable} from 'rx'
 import combineLatestObj from 'rx-combine-latest-obj'
 
-import {div} from 'cycle-snabbdom'
+import {div, span} from 'cycle-snabbdom'
 
 // why arent these in root/index??
 import 'normalize-css'
@@ -26,38 +26,160 @@ const _tabs = [
   {path: '/find', label: 'Find'},
 ]
 
-const NavContent = sources => ({
-  DOM: Observable.just(div({},'nav content')),
+const Tabs = sources => ({
+  DOM: Observable.just(div({style: {border: '2px solid green'}},'tabs')),
 })
 
-export default sources => {
-  const appBar = AppBar(sources) // will need to pass auth
-  const tabBar = TabBar({...sources, tabs: Observable.just(_tabs)})
-  const navContent = NavContent(sources)
+const Title = sources => ({
+  DOM: sources.isMobile$
+    .map(isMobile =>
+      div({style: {border: '2px solid red'}},['Page Title', isMobile ? sources.tabsDOM : null])
+    ),
+})
 
-  const page$ = nestedComponent(sources.router.define(_routes),sources)
+const Nav = sources => ({
+  DOM: sources.isMobile$
+    .map(isMobile =>
+      div({style: {border: '2px solid blue'}},[isMobile ? null : sources.titleDOM, 'Nav Items'])
+    ),
+})
 
-  const children = [appBar,tabBar,navContent,page$]
+const Header = sources => ({
+  DOM: sources.isMobile$
+    .map(isMobile =>
+      div({style: {border: '2px solid yellow'}},[isMobile ? sources.titleDOM : sources.tabsDOM])
+    ),
+})
 
-  const closeSideNav$ = sources.DOM.select('.close-sideNav').events('click')
+// import {sideNav} from 'helpers/layout/sideNav'
+import {Col, Row} from 'snabbdom-material'
 
-  const redirectOnLogout$ = sources.auth$.filter(auth => !auth).map(() => '/')
+const defaultStyles = {
+  zIndex: '1001',
+  position: 'fixed',
+  top: '0',
+  bottom: '0',
+  overflow: 'auto',
+}
 
-  const sidenavOpen$ = appBar.navButton$.map(true)
-    .merge(closeSideNav$.map(false))
-    .startWith(false)
+import {material} from 'helpers/dom'
+import {Mask} from 'snabbdom-material'
+
+    // Mask({isOpen, material, className: 'close-sideNav'}),
+
+function renderSideNav(config, children) {
+  const {className = '', style: userStyle = {}} = config
+  const classes = ['sidenav', 'paper2', className].filter(Boolean)
+  const style = Object.assign(defaultStyles, userStyle, material.sidenav)
+  return div({},[
+    Mask({isOpen: true, material, className: 'close-sideNav'}),
+    div(`.${classes.join('.')}`, {style}, [
+      span({}, children),
+    ]),
+  ])
+}
+
+const SideNav = sources => {
+  const close$ = sources.DOM.select('.close-sideNav').events('click')
+    .map(false)
+
+  const _DOM = ({isMobile,isOpen,contentDOM}) =>
+    isMobile && isOpen && renderSideNav({}, [contentDOM]) ||
+      (!isMobile && div({},[contentDOM]) || span(''))
+      // isOpen ? div({},[
+      //   Mask({isOpen: true, material, className: 'close-sideNav'}),
+      //   renderSideNav({}, [contentDOM]),
+      // ]) : null
+      // ) || div({},[contentDOM])
 
   const DOM = combineLatestObj({
-    pageDOM$: page$.pluck('DOM'),
-    appBarDOM$: appBar.DOM,
-    tabBarDOM$: tabBar.DOM,
-    navContentDOM$: navContent.DOM,
     isMobile$: sources.isMobile$,
-    isOpen: sidenavOpen$,
-  }).map(layoutDOM)
+    isOpen$: sources.isOpen$.merge(close$),
+    contentDOM$: sources.contentDOM,
+  }).map(_DOM)
 
   return {
     DOM,
+  }
+}
+
+const AppFrame = sources => {
+  const appBar = AppBar(sources) // will need to pass auth
+
+  const sideNav = SideNav({
+    contentDOM: sources.navDOM,
+    isOpen$: appBar.navButton$.map(true).startWith(false),
+    ...sources,
+  })
+
+  const redirectOnLogout$ = sources.auth$.filter(auth => !auth).map(() => '/')
+
+  return {
+    DOM: sources.isMobile$
+      .map(isMobile =>
+        isMobile ?
+          div({},[
+            sideNav.DOM,
+            appBar.DOM,
+            sources.headerDOM,
+            sources.pageDOM,
+          ]) :
+          div({},[
+            appBar.DOM,
+            Row({},[
+              Col({type: 'xs-3'}, [sideNav.DOM]),
+              Col({type: 'xs-9'}, [sources.headerDOM, sources.pageDOM]),
+            ]),
+          ])
+      ),
+  }
+}
+
+export default sources => {
+  const page$ = nestedComponent(sources.router.define(_routes),sources)
+  const tabs = TabBar({...sources, tabs: Observable.just(_tabs)})
+  // const tabs = Tabs(sources)
+  const title = Title({tabsDOM: tabs.DOM, ...sources})
+  const nav = Nav({titleDOM: title.DOM, ...sources})
+  const header = Header({titleDOM: title.DOM, tabsDOM: tabs.DOM, ...sources})
+
+  const appFrame = AppFrame({
+    navDOM: nav.DOM,
+    headerDOM: header.DOM,
+    pageDOM: page$.pluck('DOM'),
+    ...sources,
+  })
+
+// --
+
+  // const appBar = AppBar(sources) // will need to pass auth
+  // const tabBar = TabBar({...sources, tabs: Observable.just(_tabs)})
+  // const navContent = NavContent(sources)
+
+  // const page$ = nestedComponent(sources.router.define(_routes),sources)
+
+  // const children = [appBar,tabBar,navContent,page$]
+  const children = [appFrame, page$]
+
+  // const closeSideNav$ = sources.DOM.select('.close-sideNav').events('click')
+
+  const redirectOnLogout$ = sources.auth$.filter(auth => !auth).map(() => '/')
+
+  // const sidenavOpen$ = appBar.navButton$.map(true)
+    // .merge(closeSideNav$.map(false))
+    // .startWith(false)
+
+  // const DOM = combineLatestObj({
+  //   pageDOM$: page$.pluck('DOM'),
+  //   appBarDOM$: appBar.DOM,
+  //   tabBarDOM$: tabBar.DOM,
+  //   navContentDOM$: navContent.DOM,
+  //   isMobile$: sources.isMobile$,
+  //   isOpen: sidenavOpen$,
+  // }).map(layoutDOM)
+
+  return {
+    DOM: appFrame.DOM,
     queue$: mergeOrFlatMapLatest('queue$', ...children),
     route$: redirectOnLogout$.merge(
       mergeOrFlatMapLatest('route$', ...children),
