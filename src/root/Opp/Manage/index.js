003f0d4dacd65520c487a56c95e38ecd54e8d18e
Stevo <sdebaun@sparks.network>
2016-03-21 05:00:23 -0700

    wat failed merge?

--- src/root/Opp/Manage/index.js
+++ src/root/Opp/Manage/index.js
@@ -17,10 +17,11 @@ import {nestedComponent, mergeOrFlatMapLatest} from 'util'
 // import {rows, log} from 'util'
 
 import Describe from './Describe'
+import Exchange from './Exchange'
 import Applying from './Applying'
 
 // const Describe = ComingSoon('Opp/Glance/Describe')
-const Exchange = ComingSoon('Opp/Glance/Exchange')
+// const Exchange = ComingSoon('Opp/Glance/Exchange')
 // const Applying = ComingSoon('Opp/Glance/Applying')
 
 const _routes = {
