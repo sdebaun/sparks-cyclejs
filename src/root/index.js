c8538389b1a6ce72652a269df429fb86f4dc2233
Stevo <sdebaun@sparks.network>
2016-03-21 02:19:15 -0700

    refactor root/index

--- src/root/index.js
+++ src/root/index.js
@@ -1,4 +1,6 @@
 import {Observable} from 'rx'
+const {just, empty} = Observable
+
 import isolate from '@cycle/isolate'
 
 import Landing from './Landing'
@@ -16,11 +18,9 @@ import '!style!css!snabbdom-material/lib/index.css'
 
 import {nestedComponent} from 'util'
 
-import {log} from 'util'
+// import {log} from 'util'
 
 import './styles.scss'
-
-const just = Observable.just
 
 // Route definitions at this level
 const routes = {
@@ -40,40 +40,8 @@ const routes = {
     isolate(Engagement)({engagementKey$: just(key), ...sources}),
 }
 
-export default sources => {
-  // get queue response stream based on auth'd uid
-  const responses$ = sources.auth$
-    .flatMapLatest(auth => auth ? sources.queue$(auth.uid) : Observable.empty())
-    .pluck('val')
-    .share()
-
-  // lots of components like to know the user's profile key and profile info
-  // let us make it easy for them
-  const userProfileKey$ = sources.auth$
-    .flatMapLatest(auth =>
-      auth ? sources.firebase('Users', auth.uid) : Observable.just(null)
-    )
-
-  const userProfile$ = userProfileKey$
-    .distinctUntilChanged()
-    .flatMapLatest(key => {
-      return key ? sources.firebase('Profiles', key) : Observable.just(null)
-    })
-
-  // sources.auth$.subscribe(log('auth$'))
-  // userProfileKey$.subscribe(log('userProfileKey$'))
-  // userProfile$.subscribe(log('userProfile$'))
-
-  // unfortunately this does not gather global uses of navlink
-  // i suspect its because of how the DOM driver uses isolate()
-  // to intentionally ignore isolated events
-  const navRouting$ = sources.DOM.select('.navLink').events('click')
-    .map(e => e.ownerTarget.dataset.link)
-
-  // these two redirects are passed on to child components
-  // who simply plug them into their route$ sink
-  // if they want this behavior
-  const redirectLogin$ = userProfile$
+const AuthRedirectManager = sources => {
+  const redirectLogin$ = sources.userProfile$
     .filter(profile => !!profile)
     .map(profile => profile.isAdmin ? '/admin' : '/dash')
 
@@ -81,38 +49,86 @@ export default sources => {
     .filter(profile => !profile)
     .map(() => '/')
 
-  // this is the only global redirect
-  // it always gets piped to the router
-  const redirectUnconfirmed$ = userProfileKey$
+  // this is the only global redirect, always gets piped to the router
+  const redirectUnconfirmed$ = sources.userProfileKey$
     .withLatestFrom(sources.auth$)
     .filter(([profile,auth]) => !profile && !!auth)
     .map(() => '/confirm')
 
-  const page$ = nestedComponent(sources.router.define(routes),{
-    responses$, userProfileKey$, userProfile$, redirectLogin$, redirectLogout$,
-    ...sources,
-  })
-
-  const authedActions$ = page$
-    .flatMapLatest(({queue$}) => queue$ || Observable.never())
-    .withLatestFrom(sources.auth$)
-    // .do(log('authedAction$'))
-    .map(([action,auth]) => ({uid: auth && auth.uid, ...action}))
-
-  authedActions$.subscribe(log('authedActions$'))
-
-  const router = Observable.merge(
-    page$.flatMapLatest(({route$}) => route$ || Observable.never()),
+  return {
+    redirectLogin$,
+    redirectLogout$,
     redirectUnconfirmed$,
-    navRouting$,
-  )
+  }
+}
 
-  router.subscribe(log('router'))
+const UserManager = sources => {
+  const userProfileKey$ = sources.auth$
+    .flatMapLatest(auth =>
+      auth ? sources.firebase('Users', auth.uid) : just(null)
+    )
+
+  const userProfile$ = userProfileKey$
+    .distinctUntilChanged()
+    .flatMapLatest(key => {
+      return key ? sources.firebase('Profiles', key) : just(null)
+    })
 
   return {
-    DOM: page$.flatMapLatest(({DOM}) => DOM),
-    auth$: page$.flatMapLatest(({auth$}) => auth$ || Observable.never()),
-    queue$: authedActions$,
+    userProfile$,
+    userProfileKey$,
+  }
+}
+
+const AuthedResponseManager = sources => {
+  const responses$ = sources.auth$
+    .flatMapLatest(auth => auth ? sources.queue$(auth.uid) : empty())
+    .pluck('val')
+    .share()
+
+  return {responses$}
+}
+
+const AuthedActionManager = sources => {
+  const queue$ = sources.queue$
+    .withLatestFrom(sources.auth$)
+    .map(([action,auth]) => ({uid: auth && auth.uid, ...action}))
+
+  return {queue$}
+}
+
+export default sources => {
+  const user = UserManager(sources)
+
+  const redirects = AuthRedirectManager({...user, ...sources})
+
+  const {responses$} = AuthedResponseManager(sources)
+
+  const page$ = nestedComponent(sources.router.define(routes), {
+    ...sources,
+    ...user,
+    ...redirects,
+    responses$,
+  })
+
+  const DOM = page$.pluckFlat('DOM')
+
+  const auth$ = page$.pluckFlat('auth$')
+
+  const {queue$} = AuthedActionManager({
+    ...sources,
+    queue$: page$.pluckFlat('queue$'), // must be 2d arg to override sources
+  })
+
+  const router = Observable.merge(
+    page$.pluckFlat('route$'),
+    redirects.redirectUnconfirmed$,
+  )
+
+  return {
+    DOM,
+    auth$,
+    queue$,
     router,
   }
 }
