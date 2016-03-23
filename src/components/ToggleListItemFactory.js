3ad904d0cfbd4cb73f68de11d76e21d1558c120d
Stevo <sdebaun@sparks.network>
2016-03-23 08:47:37 -0700

    toggle control

--- src/components/ToggleListItemFactory.js
+++ src/components/ToggleListItemFactory.js
@@ -1,38 +1,47 @@
 import {Observable} from 'rx'
+const {just} = Observable
+
 import combineLatestObj from 'rx-combine-latest-obj'
 
+import {div} from 'helpers'
 import listItem from 'helpers/listItem'
 import {icon} from 'helpers'
+import {ToggleControl} from 'components/sdm'
 
 // import {col} from 'helpers'
 
 import {log} from 'util'
 
 export default ({labelTrue, labelFalse}) => sources => {
+  const toggle = ToggleControl(sources)
+
   // render is nested so it can use factory args
-  const _render = ({value}) =>
+  const _render = ({value, toggleDOM}) =>
     listItem({
-      iconDOM: value ? icon('toggle-on','green') : icon('toggle-off','#333'),
+      iconDOM: toggleDOM,
       title: value ? labelTrue : labelFalse,
       clickable: true,
     })
 
-  const toggleClick$ = sources.DOM.select('.clickable').events('click')
+  // const toggleClick$ = sources.DOM.select('.clickable').events('click')
+  const value$ = toggle.value$
 
-  toggleClick$.subscribe(log('toggleClick$'))
+  // toggleClick$.subscribe(log('toggleClick$'))
 
-  const sourceValue$ = sources.value$ || Observable.just(false)
+  // const sourceValue$ = sources.value$ || Observable.just(false)
 
-  const value$ = sourceValue$
-    .sample(toggleClick$)
-    .map(value => !value)
+  // const value$ = sourceValue$
+  //   .sample(toggleClick$)
+  //   .map(value => !value)
 
   value$.subscribe(log('value$'))
 
   const viewState = {
-    value$: sourceValue$,
+    value$: sources.value$ || just(false),
+    toggleDOM$: toggle.DOM,
+    // toggleDOM$: just(div({},['wat'])),
   }
   const DOM = combineLatestObj(viewState).map(_render)
-
+  // const DOM = just(div({},['wat']))
   return {DOM, value$}
 }
---
+++ src/components/sdm/ToggleControl.js
@@ -0,0 +1,41 @@
+import {Observable} from 'rx'
+const {just} = Observable
+
+import combineLatestObj from 'rx-combine-latest-obj'
+
+import {div} from 'helpers'
+import {icon} from 'helpers'
+
+// import {log} from 'util'
+
+const ToggleControl = sources => {
+  const click$ = sources.DOM.select('.toggle').events('click')
+
+  const sourceValue$ = (sources.value$ || Observable.just(false))
+    .startWith(false)
+
+  const value$ = sourceValue$
+    .sample(click$)
+    .map(value => !value)
+
+  const renderValue$ = sourceValue$.merge(value$)
+
+  const viewState = {
+    value$: renderValue$,
+    classNames$: sources.classNames$ || just([]),
+  }
+
+  const DOM = combineLatestObj(viewState)
+    .map(({value}) =>
+      div({class: {toggle: true}},[
+        value ? icon('toggle-on','green') : icon('toggle-off','#333'),
+      ])
+    )
+
+  return {
+    DOM,
+    value$,
+  }
+}
+
+export {ToggleControl}
