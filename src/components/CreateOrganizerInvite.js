a7e12f06615b609c1bab1469d0c8d34859839527
Stevo <sdebaun@sparks.network>
2016-03-26 03:39:38 -0700

    fixing for new ListItem stuff

--- src/components/CreateOrganizerInvite.js
+++ src/components/CreateOrganizerInvite.js
@@ -1,68 +1,105 @@
 import {Observable} from 'rx'
-import combineLatestObj from 'rx-combine-latest-obj'
+const {just} = Observable
 
 // import isolate from '@cycle/isolate'
 
 import {Organizers} from 'remote'
 
+// import {OppForm} from 'components/opp'
+import {ListItemWithDialog} from 'components/sdm'
 import {OrganizerInviteForm} from 'components/OrganizerInviteForm'
 
-import {col} from 'helpers'
-import modal from 'helpers/modal'
-import listItem from 'helpers/listItem'
+const CreateOrganizerListItem = sources => {
+  const form = OrganizerInviteForm(sources)
 
-// import {log} from 'util'
+  const listItem = ListItemWithDialog({...sources,
+    iconName$: just('person_add'),
+    title$: just('Invite another Organizer to help you run the project.'),
+    dialogTitleDOM$: just('Invite Organizer'),
+    dialogContentDOM$: form.DOM,
+  })
 
-const _openActions$ = ({DOM}) => Observable.merge(
-  DOM.select('.open').events('click').map(true),
-  DOM.select('.close').events('click').map(false),
-)
-
-const _submitAction$ = ({DOM}) =>
-  DOM.select('.submit').events('click').map(true)
-
-const _render = ({isOpen, organizerInviteFormDOM}) =>
-  col(
-    listItem({
-      iconName: 'person_add',
-      iconBackgroundColor: 'yellow',
-      title: 'Invite Organizer',
-      className: 'open',
-      clickable: true,
-    }),
-    modal({
-      isOpen,
-      title: 'Invite Organizer',
-      iconName: 'person_add',
-      submitLabel: 'Bring Em On',
-      closeLabel: 'Not Yet',
-      content: organizerInviteFormDOM,
-    })
-  )
-
-export default sources => {
-  const organizerInviteForm = OrganizerInviteForm(sources)
-
-  const submit$ = _submitAction$(sources)
-
-  const queue$ = organizerInviteForm.item$
-    .sample(submit$)
-    .zip(sources.projectKey$,
-      (organizer,projectKey) => ({projectKey, ...organizer})
-    )
+  const queue$ = form.item$
+    .sample(listItem.submit$)
+    .zip(sources.projectKey$, (opp,projectKey) => ({projectKey, ...opp}))
     .map(Organizers.create)
 
-  const isOpen$ = _openActions$(sources)
-    .merge(submit$.map(false))
-    .startWith(false)
-
-  const viewState = {
-    isOpen$,
-    project$: sources.project$,
-    organizerInviteFormDOM$: organizerInviteForm.DOM,
+  return {
+    DOM: listItem.DOM,
+    queue$,
   }
-
-  const DOM = combineLatestObj(viewState).map(_render)
-
-  return {DOM, queue$}
 }
+
+export default CreateOrganizerListItem
+
+///
+
+
+// import {Observable} from 'rx'
+// import combineLatestObj from 'rx-combine-latest-obj'
+
+// // import isolate from '@cycle/isolate'
+
+// import {Organizers} from 'remote'
+
+// import {OrganizerInviteForm} from 'components/OrganizerInviteForm'
+
+// import {col} from 'helpers'
+// import modal from 'helpers/modal'
+// import listItem from 'helpers/listItem'
+
+// // import {log} from 'util'
+
+// const _openActions$ = ({DOM}) => Observable.merge(
+//   DOM.select('.open').events('click').map(true),
+//   DOM.select('.close').events('click').map(false),
+// )
+
+// const _submitAction$ = ({DOM}) =>
+//   DOM.select('.submit').events('click').map(true)
+
+// const _render = ({isOpen, organizerInviteFormDOM}) =>
+//   col(
+//     listItem({
+//       iconName: 'person_add',
+//       iconBackgroundColor: 'yellow',
+//       title: 'Invite Organizer',
+//       className: 'open',
+//       clickable: true,
+//     }),
+//     modal({
+//       isOpen,
+//       title: 'Invite Organizer',
+//       iconName: 'person_add',
+//       submitLabel: 'Bring Em On',
+//       closeLabel: 'Not Yet',
+//       content: organizerInviteFormDOM,
+//     })
+//   )
+
+// export default sources => {
+//   const organizerInviteForm = OrganizerInviteForm(sources)
+
+//   const submit$ = _submitAction$(sources)
+
+//   const queue$ = organizerInviteForm.item$
+//     .sample(submit$)
+//     .zip(sources.projectKey$,
+//       (organizer,projectKey) => ({projectKey, ...organizer})
+//     )
+//     .map(Organizers.create)
+
+//   const isOpen$ = _openActions$(sources)
+//     .merge(submit$.map(false))
+//     .startWith(false)
+
+//   const viewState = {
+//     isOpen$,
+//     project$: sources.project$,
+//     organizerInviteFormDOM$: organizerInviteForm.DOM,
+//   }
+
+//   const DOM = combineLatestObj(viewState).map(_render)
+
+//   return {DOM, queue$}
+// }
