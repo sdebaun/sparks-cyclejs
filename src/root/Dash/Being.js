4f62d457c5c8827b0669a2f9c4eab43360ad33e9
Steve <stevenmathews.nz@gmail.com>
2016-03-30 17:23:35 +1300

    Add profile info

--- src/root/Dash/Being.js
+++ src/root/Dash/Being.js
@@ -1,9 +1,29 @@
+import {h, ul} from 'cycle-snabbdom'
+import {Observable} from 'rx'
+const {combineLatest} = Observable
+import {div} from 'helpers'
 import {LargeProfileAvatar} from 'components/profile'
 
-export default sources => {
-  const portraitUrl$ = sources.userProfile$.map(up => up && up.portraitUrl)
+const ProfileInfo = sources => ({
+  DOM: sources.userProfile$
+    .map(profile =>
+    ul({}, [
+      h('h4', {}, `${profile.fullName}`),
+      h('p', {}, `${profile.email}`),
+      h('p', {}, `${profile.phone}`),
+    ])
+  ),
+})
 
+export default sources => {
+  const portraitUrl$ = sources.userProfile$
+    .map(up => up && up.portraitUrl)
+  const profileDOM = combineLatest(
+    LargeProfileAvatar({...sources, src$: portraitUrl$}).DOM,
+    ProfileInfo(sources).DOM,
+    (...doms) => div({},doms),
+  )
   return {
-    DOM: LargeProfileAvatar({...sources, src$: portraitUrl$}).DOM,
+    DOM: profileDOM,
   }
 }
