2429828be059939cd3ee25d35f29293de18b3ca2
Steve <stevenmathews.nz@gmail.com>
2016-04-01 15:57:32 +1300

    Getting text area to work

--- src/root/Dash/Being.js
+++ src/root/Dash/Being.js
@@ -1,9 +1,14 @@
 import {h, ul} from 'cycle-snabbdom'
 import {Observable} from 'rx'
 const {combineLatest, of} = Observable
+import isolate from '@cycle/isolate'
 import {div} from 'helpers'
 // import {RaisedButton} from 'components/sdm'
 import {LargeProfileAvatar} from 'components/profile'
+import {TextAreaControl} from 'components/sdm/TextAreaControl'
+import {
+  ListItemCollapsibleTextArea,
+} from 'components/sdm'
 
 const ProfileInfo = sources => ({
   DOM: sources.userProfile$
@@ -36,10 +41,48 @@ const Profile = sources => {
   return {DOM}
 }
 
+const Intro = sources => {
+  const introText = TextAreaControl(sources)
+  const DOM = introText.DOM
+    .map(intro =>
+      div({}, [
+        h('p', {},`
+          Give people a quick glimpse of how awesome you are.
+          This is the first thing they see on your applications.
+        `),
+        intro,
+      ])
+    )
+
+  return {DOM}
+}
+
+const DescriptionTextarea = sources => ListItemCollapsibleTextArea({
+  ...sources,
+  title$: of('Introduce Yourself!'),
+  subtitle$: of(`
+    Give people a quick glimpse of how awesome you are.
+    This is the first thing they see on your applications.
+  `),
+  iconName$: of('playlist_add'),
+  okLabel$: of('yes do it'),
+  cancelLabel$: of('wait a sec'),
+})
+
 export default sources => {
-  const profileDOM = Profile(sources).DOM
+  const profile = Profile(sources)
+  const intro = Intro(sources)
+  const DOM = combineLatest(
+    profile.DOM,
+    intro.DOM,
+    (...doms) => div({},doms)
+  )
+
+  const descriptionTextarea = isolate(DescriptionTextarea)({...sources,
+    value$: sources.project$.pluck('description'),
+  })
 
   return {
-    DOM: profileDOM,
+    DOM: descriptionTextarea.DOM,
   }
 }
