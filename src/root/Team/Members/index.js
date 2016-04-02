755ec51c4a780a2f56036416717a950f3f930c8d
Stevo <sdebaun@sparks.network>
2016-04-02 15:45:49 -0700

    start members page

---
+++ src/root/Team/Members/index.js
@@ -0,0 +1,55 @@
+import {Observable} from 'rx'
+import TabBar from 'components/TabBar'
+import ComingSoon from 'components/ComingSoon'
+
+import {nestedComponent, mergeOrFlatMapLatest} from 'util'
+
+// import Describe from './Describe'
+// import Applying from './Applying'
+
+const Confirmed = ComingSoon('Manage/Glance/Confirmed')
+const Applied = ComingSoon('Manage/Glance/Applied')
+const Approved = ComingSoon('Manage/Glance/Approved')
+
+const _routes = {
+  '/': Confirmed,
+  '/applied': Applied,
+  '/approved': Approved,
+}
+
+const _tabs = [
+  {path: '/', label: 'Confirmed'},
+  {path: '/applied', label: 'Applied'},
+  {path: '/approved', label: 'ApplApprovedying'},
+]
+
+export default sources => {
+  const page$ = nestedComponent(
+    sources.router.define(_routes), sources,
+  )
+
+  const tabBar = TabBar({...sources, tabs: Observable.just(_tabs)})
+
+  const children = [page$, tabBar]
+
+  const DOM = page$.flatMapLatest(page => page.DOM)
+
+  const tabBarDOM = tabBar.DOM
+
+  const pageTitle = Observable.just('Manage')
+
+  const auth$ = mergeOrFlatMapLatest('auth$', ...children)
+
+  const queue$ = mergeOrFlatMapLatest('queue$', ...children)
+
+  const route$ = mergeOrFlatMapLatest('route$', ...children)
+
+  return {
+    DOM,
+    tabBarDOM,
+    pageTitle,
+    auth$,
+    queue$,
+    route$,
+  }
+}
