10481fdd9c1cf040b370678393cf24b3c418d85d
Stevo <sdebaun@sparks.network>
2016-04-04 00:54:01 -0700

    TabbedPage in Opp

--- src/root/Opp/Engaged/index.js
+++ src/root/Opp/Engaged/index.js
@@ -1,55 +1,26 @@
 import {Observable} from 'rx'
-import TabBar from 'components/TabBar'
+const {of} = Observable
+
 import ComingSoon from 'components/ComingSoon'
-
-import {nestedComponent, mergeOrFlatMapLatest} from 'util'
-
-// import Describe from './Describe'
-import Applied from './Applied'
+import {TabbedPage} from 'components/ui'
 
 const Confirmed = ComingSoon('Manage/Glance/Confirmed')
-// const Applied = ComingSoon('Manage/Glance/Applied')
+import Applied from './Applied'
 const Accepted = ComingSoon('Manage/Glance/Accepted')
 
-const _routes = {
-  '/': Confirmed,
-  '/applied': Applied,
-  '/accepted': Accepted,
-}
+export default sources => ({
+  pageTitle: of('At a Glance'),
 
-const _tabs = [
-  {path: '/', label: 'Confirmed'},
-  {path: '/applied', label: 'Applied'},
-  {path: '/accepted', label: 'Accepted'},
-]
-
-export default sources => {
-  const page$ = nestedComponent(
-    sources.router.define(_routes), sources,
-  )
-
-  const tabBar = TabBar({...sources, tabs: Observable.just(_tabs)})
-
-  const children = [page$, tabBar]
-
-  const DOM = page$.flatMapLatest(page => page.DOM)
-
-  const tabBarDOM = tabBar.DOM
-
-  const pageTitle = Observable.just('Manage')
-
-  const auth$ = mergeOrFlatMapLatest('auth$', ...children)
-
-  const queue$ = mergeOrFlatMapLatest('queue$', ...children)
-
-  const route$ = mergeOrFlatMapLatest('route$', ...children)
-
-  return {
-    DOM,
-    tabBarDOM,
-    pageTitle,
-    auth$,
-    queue$,
-    route$,
-  }
-}
+  ...TabbedPage({...sources,
+    tabs$: of([
+      {path: '/', label: 'Confirmed'},
+      {path: '/applied', label: 'Applied'},
+      {path: '/accepted', label: 'Accepted'},
+    ]),
+    routes$: of({
+      '/': Confirmed,
+      '/applied': Applied,
+      '/accepted': Accepted,
+    }),
+  }),
+})
