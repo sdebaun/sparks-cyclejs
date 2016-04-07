6b6f549d9f8b5505ce2e3dd9aabcbe3ebe678fe6
Steve <stevenmathews.nz@gmail.com>
2016-04-07 15:05:43 +1200

    Use nightwatch mocha as the test runner

--- tests/e2e/test.js
+++ tests/e2e/test.js
@@ -1,13 +1,27 @@
-module.exports = {
-  'Demo test Google' : function (browser) {
-    browser
-      .url('http://www.google.com')
-      .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
-      .pause(1000)
-      .assert.containsText('#main', 'Night Watch')
-      .end();
-  }
-};
+describe('Google demo test for Mocha', function() {
+  describe('with Nightwatch', function() {
+    before(function(client, done) {
+      done()
+    })
+    after(function(client, done) {
+      client.end(function() {
+        done()
+      })
+    })
+    afterEach(function(client, done) {
+      done()
+    })
+    beforeEach(function(client, done) {
+      done()
+    })
+    it('uses BDD to run the Google simple test', function(client) {
+      client
+        .url('http://google.com')
+        .expect.element('body').to.be.present.before(1000)
+
+      client.setValue('input[type=text]', ['nightwatch', client.Keys.ENTER])
+        .pause(1000)
+        .assert.containsText('#main', 'Night Watch')
+    })
+  })
+})
