20d269356703fe26d81d6e16a29acb88b3237326
Steve <stevenmathews.nz@gmail.com>
2016-04-08 15:09:27 +1200

    Create login test

--- test/integration/test.js
+++ test/integration/test.js
@@ -1,27 +1,35 @@
-describe('Google demo test for Mocha', function() {
-  describe('with Nightwatch', function() {
-    before(function(client, done) {
+describe('Nightwatch integration testing', function() {
+  before(function(client, done) {
+    done()
+  })
+  after(function(client, done) {
+    client.end(function() {
       done()
-    })
-    after(function(client, done) {
-      client.end(function() {
-        done()
-      })
-    })
-    afterEach(function(client, done) {
-      done()
-    })
-    beforeEach(function(client, done) {
-      done()
-    })
-    it('uses BDD to run the Google simple test', function(client) {
-      client
-        .url('http://google.com')
-        .expect.element('body').to.be.present.before(1000)
-
-      client.setValue('input[type=text]', ['nightwatch', client.Keys.ENTER])
-        .pause(1000)
-        .assert.containsText('#main', 'Night Watch')
     })
   })
+  afterEach(function(client, done) {
+    done()
+  })
+  beforeEach(function(client, done) {
+    done()
+  })
+  it('shows dash after login', function(client) {
+    client
+      .url('http://localhost:8080')
+      .click('i.icon-more_vert')
+      .click('div.title')
+      .window_handles((result) => {
+        client.switchWindow(result.value[1])
+          .setValue('#Email', 'test@sparks.network')
+          .click('#next')
+          .pause(500)
+          .setValue('#Passwd', 'sparks4life')
+          .click('#signIn')
+        })
+      .window_handles(function(result) {
+        client.switchWindow(result.value[0])
+          .pause(2500)
+          .assert.containsText('.title', 'Test User')
+        })
+      })
 })
