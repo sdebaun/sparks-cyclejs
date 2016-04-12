890afa5b121274d66442c4bef9913dbd1872881a
Steve <stevenmathews.nz@gmail.com>
2016-04-12 16:04:40 +1200

    Create date picker

--- src/root/Team/Schedule/Overview.js
+++ src/root/Team/Schedule/Overview.js
@@ -1,9 +1,16 @@
 import {Observable} from 'rx'
 const {of} = Observable
-import {h} from 'cycle-snabbdom'
+import {InputControl} from 'components/sdm'
+import {RaisedButton} from 'components/sdm'
+import {combineLatestToDiv} from 'util'
 
-export default () => {
+export default (sources) => {
+  const ic = InputControl({
+    label$: of('Choose a day to start adding shifts! (YYYY-MM-DD)'), ...sources,
+  })
+  const rb = RaisedButton({label$: of('Add Date'), ...sources})
   return {
-    DOM: of(h('p', {}, 'Choose a day to start adding shifts!')),
+    DOM: combineLatestToDiv(ic.DOM, rb.DOM),
+    value$: ic.value$.sample(rb.click$),
   }
 }
