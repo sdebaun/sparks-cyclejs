5f685f2d31237954a7e95011685eb0cde53fafab
Steve <stevenmathews.nz@gmail.com>
2016-04-15 13:28:09 +1200

    Create bonus toggle

--- src/root/Team/Schedule/Shifts.js
+++ src/root/Team/Schedule/Shifts.js
@@ -3,7 +3,10 @@ const {of} = Observable
 import {ListItemWithDialog} from 'components/sdm'
 import {Form} from 'components/ui/Form'
 import {InputControl} from 'components/sdm'
+import {ListItemClickable} from 'components/sdm'
 import {Shifts} from 'components/remote'
+import {div} from 'cycle-snabbdom'
+import {icon} from 'helpers'
 
 const StartsInput = sources => InputControl({...sources,
   label$: of('Starts At Hour (24 hour)'),
@@ -17,11 +20,53 @@ const PeopleInput = sources => InputControl({...sources,
   label$: of('People (Number)'),
 })
 
+const ToggleControl = sources => {
+  const click$ = sources.DOM.select('.toggle').events('click')
+    .map(true)
+    .scan((a) => !a, false)
+    .startWith(false)
+    .shareReplay(1)
+
+  return {
+    click$,
+    DOM: click$.map(v =>
+      div({class: {toggle: true}},[
+        v ?
+        icon('toggle-on','accent') :
+        icon('toggle-off'),
+      ])
+    ),
+  }
+}
+
+const ListItemBonusToggle = (sources) => {
+  const toggle = ToggleControl(sources)
+  const item = ListItemClickable({...sources,
+    leftDOM$: toggle.DOM,
+    title$: toggle.click$.flatMapLatest(v =>
+      v ? sources.titleTrue$ : sources.titleFalse$
+    ),
+  })
+
+  return {
+    DOM: item.DOM,
+    value$: toggle.click$,
+  }
+}
+
+const ToggleBonus = sources => ListItemBonusToggle({...sources,
+  titleTrue$:
+    of('Bonus'),
+  titleFalse$:
+    of('Normal'),
+})
+
 const ShiftForm = sources => Form({...sources,
   Controls$: of([
     {field: 'starts', Control: StartsInput},
     {field: 'hours', Control: HoursInput},
     {field: 'people', Control: PeopleInput},
+    {field: 'bonus', Control: ToggleBonus},
   ]),
 })
 
@@ -35,9 +80,10 @@ const AddShift = sources => {
     dialogContentDOM$: form.DOM,
   })
 
+  const submit$ = liwd.submit$
   const queue$ = form.item$
-    .sample(liwd.submit$)
-    .zip(sources.teamKey$, (shift,teamKey) => ({teamKey, ...shift}))
+    .withLatestFrom(sources.teamKey$, (shift, teamKey) => ({teamKey, ...shift}))
+    .sample(submit$)
     .map(Shifts.action.create)
 
   return {
