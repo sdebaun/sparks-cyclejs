e8400efef13a3d18c4831ddcc9c84f7415204eb7
Stevo <sdebaun@sparks.network>
2016-04-15 01:58:34 -0700

    moar purge placeholder tabs

--- src/root/Opp/Engaged/index.js
+++ src/root/Opp/Engaged/index.js
@@ -1,26 +1,23 @@
 import {Observable} from 'rx'
 const {of} = Observable
 
-import ComingSoon from 'components/ComingSoon'
 import {TabbedPage} from 'components/ui'
 
-const Confirmed = ComingSoon('Manage/Glance/Confirmed')
 import Applied from './Applied'
-const Accepted = ComingSoon('Manage/Glance/Accepted')
 
 export default sources => ({
   pageTitle: of('Engaged Volunteers'),
 
   ...TabbedPage({...sources,
     tabs$: of([
-      {path: '/', label: 'Confirmed'},
-      {path: '/applied', label: 'Applied'},
-      {path: '/accepted', label: 'Accepted'},
+      {path: '/', label: 'Applied'},
+      // {path: '/applied', label: 'Applied'},
+      // {path: '/accepted', label: 'Accepted'},
     ]),
     routes$: of({
-      '/': Confirmed,
-      '/applied': Applied,
-      '/accepted': Accepted,
+      '/': Applied,
+      // '/applied': Applied,
+      // '/accepted': Accepted,
     }),
   }),
 })
