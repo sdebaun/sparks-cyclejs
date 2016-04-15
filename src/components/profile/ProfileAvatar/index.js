548a85cb97c4f534aa527e63ed490c370a379c57
Stevo <sdebaun@sparks.network>
2016-04-14 19:13:26 -0700

    cleanup unused

--- src/components/profile/ProfileAvatar/index.js
+++ src/components/profile/ProfileAvatar/index.js
@@ -1,30 +1,3 @@
-// require('./styles.scss')
-
-// import {Observable} from 'rx'
-// const {just, combineLatest} = Observable
-
-// import {img} from 'cycle-snabbdom'
-
-// const CLASSES = {avatar: true}
-// const MEDIUM = {medium: true}
-// const LARGE = {large: true}
-
-// const Avatar = sources => ({
-//   DOM: combineLatest(
-//     sources.classes$ || just({}),
-//     sources.src$,
-//     (classes, src) => img({class: {...CLASSES, ...classes}, attrs: {src}})
-//   ),
-// })
-
-// const MediumAvatar = ({classes$, ...sources}) => Avatar({...sources,
-//   classes$: classes$ ? classes$.map(c => ({...MEDIUM, ...c})) : just(MEDIUM),
-// })
-
-// const LargeAvatar = ({classes$, ...sources}) => Avatar({...sources,
-//   classes$: classes$ ? classes$.map(c => ({...LARGE, ...c})) : just(MEDIUM),
-// })
-
 import {
   Avatar,
   MediumAvatar,
@@ -35,6 +8,7 @@ import {
   Profiles,
 } from 'components/remote'
 
+// hmmm???
 const ProfileFetcher = sources => ({
   profile$: sources.profileKey$
     .flatMapLatest(Profiles.query.one(sources)),
@@ -58,7 +32,7 @@ const LargeProfileAvatar = sources => LargeAvatar({...sources,
 })
 
 export {
-  LargeProfileAvatar,
-  MediumProfileAvatar,
   ProfileAvatar,
+  MediumProfileAvatar,
+  LargeProfileAvatar,
 }
