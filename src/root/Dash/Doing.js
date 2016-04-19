aa7a44ad5f59fd9f5d13cf0a242755222c07c987
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-04-19 04:58:18 -0400

    feat(Dash/Doing): implement confirm card for dash page
    When your application has been accepted a card will show up asking you to confirm your
    spot

--- src/root/Dash/Doing.js
+++ src/root/Dash/Doing.js
@@ -3,13 +3,14 @@ const {just, combineLatest} = Observable
 
 import isolate from '@cycle/isolate'
 
-import {div} from 'helpers'
+import {div, p} from 'cycle-snabbdom'
 // import {log} from 'util'
 
 import {
   Projects,
   Engagements,
   Organizers,
+  ProjectImages,
 } from 'components/remote'
 
 import {ProjectItem, ProjectForm} from 'components/project'
@@ -23,6 +24,7 @@ import {
   RaisedButton,
   FlatButton,
   Dialog,
+  CardNavigating,
 } from 'components/sdm'
 
 const ManagedProjectsHeader = () => ListItem({
@@ -85,7 +87,7 @@ const OrganizingItem = sources => {
   const project$ = projectKey$
     .flatMapLatest(Projects.query.one(sources))
     // bc ProjectItem expects its item$ to have a $key
-    .combineLatest(projectKey$, (p,$key) => ({...p, $key}))
+    .combineLatest(projectKey$, (_p,$key) => ({..._p, $key}))
 
   return ProjectItem({...sources,
     subtitle$: sources.item$.pluck('authority'),
@@ -163,6 +165,48 @@ const Welcome = sources => ({...sources,
   ),
 })
 
+const sparkly = require('images/pitch/sparklerHeader-2048.jpg')
+
+const _label = ({isApplied, isAccepted, isConfirmed}) =>
+  isConfirmed && 'Confirmed' ||
+    isAccepted && 'Accepted' ||
+      isApplied && 'Applied' ||
+        'Unknown'
+
+const acceptedCardView = sources => item =>
+  div({}, [
+    p({style: {fontSize: '1em', fontWeight: 'bold'}},
+      'Congrats! You\'ve been accepted. Confirm to lock your spot for:'),
+    ListItem({...sources,
+      iconSrc$: just(item.opp.projectKey)
+        .flatMapLatest(ProjectImages.query.one(sources))
+        .map(i => i && i.dataUrl || '/' + sparkly),
+      title$: just(div({style: {margin: 0, padding: 0}}, [
+        p({style: {fontWeight: 'bold', margin: 0}}, [
+          item.opp.project.name,
+        ]),
+        p('.subtitle', {style: {margin: 0, fontSize: '0.8em'}}, [
+          `${item.opp.name} | ${_label(item)}`,
+        ]),
+      ])),
+    }).DOM,
+  ])
+
+const AcceptedCard = sources => CardNavigating({
+  ...sources,
+  elevation$: just(2),
+  header$: just('Confirm Now!'),
+  content$: sources.item$.map(acceptedCardView(sources)),
+  path$: sources.item$.map(i => `/engaged/${i.$key}/`),
+})
+
+const ConfirmationCards = sources =>
+  List({
+    ...sources,
+    rows$: sources.isAccepted$,
+    Control$: just(AcceptedCard),
+  })
+
 export default sources => {
   const projects$ = sources.userProfileKey$
     .flatMapLatest(Projects.query.byOwner(sources))
@@ -170,12 +214,17 @@ export default sources => {
   const engagements$ = sources.userProfileKey$
     .flatMapLatest(Engagements.query.byUser(sources))
 
+  const isAccepted$ = engagements$
+    .map(engagements => engagements.filter(e => e.isAccepted))
+
+  const confirmation = ConfirmationCards({...sources, isAccepted$})
+
   const organizers$ = sources.userProfileKey$
     .flatMapLatest(Organizers.query.byUser(sources))
 
   const welcome = Welcome({...sources,
     isVisible$: combineLatest(
-      projects$, engagements$, (p,e) => p.length === 0 && e.length === 0
+      projects$, engagements$, (_p,e) => _p.length === 0 && e.length === 0
     ),
   })
   const create = isolate(CreateProjectListItem,'create')(sources)
@@ -199,17 +248,20 @@ export default sources => {
     managed.route$,
     engaged.route$,
     organizing.route$,
+    confirmation.route$,
     Projects.redirect.create(sources).route$,
-  )
+  ).tap(x => console.log('route$', x))
 
   const DOM = combineLatest(
     sources.userProfile$.map(up => up && up.isEAP),
     create.DOM,
+    confirmation.DOM,
     welcome.DOM,
     managed.DOM,
     organizing.DOM,
     engaged.DOM,
-    (isEAP, cr, ...doms) => div({},[...doms, isEAP ? cr : null])
+    (isEAP, cr, confirm, ...doms) =>
+      div({},[confirm, ...doms, isEAP ? cr : null])
   )
 
   return {
