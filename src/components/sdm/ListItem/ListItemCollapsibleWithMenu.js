123cb67a2422848a01837eacce2a6c50758dbd8b
Stevo <sdebaun@sparks.network>
2016-04-25 01:24:01 -0700

    new listitemcollapsiblewithmenu component

--- src/components/sdm/ListItem/ListItemCollapsibleWithMenu.js
+++ src/components/sdm/ListItem/ListItemCollapsibleWithMenu.js
@@ -1,35 +1,74 @@
 import {Observable as $} from 'rx'
-import combineLatestObj from 'rx-combine-latest-obj'
 
-import {ListItemClickable} from './ListItemClickable'
+import isolate from '@cycle/isolate'
 
 import {div} from 'cycle-snabbdom'
+import {icon} from 'helpers'
 
-export const ListItemCollapsibleWithMenu = sources => {
-  const li = ListItemClickable(sources)
+import {ListItem} from './ListItem'
 
-  const isOpen$ = $.merge(
-    sources.isOpen$ || $.just(false),
-    li.click$.map(-1),
-  )
-  .scan((acc, next) => next === -1 ? !acc : next, false)
-  .startWith(false)
+import {Menu} from 'components/sdm'
+import {FlatButton} from 'components/sdm'
 
-  const viewState = {
-    isOpen$: isOpen$,
-    listItemDOM$: li.DOM,
-    contentDOM$: sources.contentDOM$ || $.just(div({},['no contentDOM$'])),
-  }
+import {combineDOMsToDiv} from 'util'
 
-  const DOM = combineLatestObj(viewState)
-    .map(({isOpen, listItemDOM, contentDOM}) =>
-      div({},[
+// should replace the one in SDM
+// and have a ListItemClickableCollapsible or somesuch for pure collapsers
+const ListItemCollapsible = sources => {
+  const isOpen$ = sources.isOpen$ || $.of(false)
+  const contentDOM$ = sources.contentDOM$ || $.of(div({},['no contentDOM$']))
+
+  const li = ListItem({...sources, classes$: $.of({clickable: true})})
+
+  const DOM = $.combineLatest(
+    isOpen$, li.DOM, contentDOM$,
+    (isOpen, listItemDOM, contentDOM) =>
+      div('.clickable',[
         listItemDOM,
         isOpen && div('.collapsible',[contentDOM]),
       ].filter(i => !!i))
-    )
+  )
 
   return {
     DOM,
   }
+}
+
+const MenuButton = sources => {
+  const btn = FlatButton({...sources, label$: $.of(icon('menu'))})
+
+  const isOpen$ = btn.click$.map(true).startWith(false)
+  const children$ = sources.menuItems$ || $.just([])
+
+  const menu = Menu({
+    ...sources,
+    isOpen$,
+    children$,
+    leftAlign$: $.of(false),
+  })
+
+  return {
+    DOM: combineDOMsToDiv('', btn, menu),
+  }
+}
+
+export const ListItemCollapsibleWithMenu = sources => {
+  const mb = isolate(MenuButton)(sources)
+
+  const click$ = $.merge(
+    sources.DOM.select('.left').events('click'),
+    sources.DOM.select('.content').events('click'),
+  ).map(-1)
+
+  const isOpen$ = $.merge(
+    sources.isOpen$ || $.just(false),
+    click$,
+  )
+  .scan((acc, next) => next === -1 ? !acc : next, false)
+  .startWith(false)
+
+  return ListItemCollapsible({...sources,
+    isOpen$,
+    rightDOM$: mb.DOM,
+  })
 }
