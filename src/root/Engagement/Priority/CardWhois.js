78466df91df5291be68d670c0359368db42d4551
Stevo <sdebaun@sparks.network>
2016-04-27 03:34:30 -0700

    link on confirmed list, show name of vol on engaged

---
+++ src/root/Engagement/Priority/CardWhois.js
@@ -0,0 +1,55 @@
+import {Observable as $} from 'rx'
+
+import isolate from '@cycle/isolate'
+
+import {icon} from 'helpers'
+import {hideable} from 'util'
+
+import {
+  TitledCard,
+  ListItem,
+} from 'components/sdm'
+
+import {
+  ToDoListItem,
+} from 'components/ui'
+
+import {
+  Profiles,
+} from 'components/remote'
+
+const ToDoShifts = sources => ToDoListItem({...sources,
+  title$: $.of('Choose when you\'d like to work.'),
+  isDone$: sources.engagement$.map(m => !!m.isAssigned),
+  path$: $.of(sources.router.createHref('/confirmation')),
+})
+
+const ToDoPayment = sources => ToDoListItem({...sources,
+  title$: $.of('Make your payments.'),
+  isDone$: sources.engagement$.map(m => !!m.isPaid),
+  path$: $.of(sources.router.createHref('/confirmation')),
+})
+
+const BaseCard = sources => {
+  const profile$ = sources.engagement$.pluck('profileKey')
+    .flatMapLatest(Profiles.query.one(sources))
+
+  const li = ListItem({...sources,
+    title$: profile$.pluck('fullName'),
+    subtitle$: profile$.map(({email, $key}) => `${email} | ${$key}`),
+  })
+
+  return TitledCard({...sources,
+    title$: $.just('Applicant View'),
+    content$: $.combineLatest(li.DOM),
+  })
+}
+
+export const CardWhois = sources => hideable(BaseCard)({...sources,
+  elevation$: $.just(2),
+  isVisible$: $.combineLatest(
+    sources.engagement$.pluck('profileKey'),
+    sources.userProfileKey$,
+    (pk,upk) => pk !== upk
+  ),
+})
