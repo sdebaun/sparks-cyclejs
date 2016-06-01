0bc1a4006f72f22dcc2f187c956f2208648e3ccb
Jeremy Wells <jemmyw@gmail.com>
2016-06-01 12:55:59 +1200

    feat(StaticSite): Don't load development stuff in production when executing express

--- index.js
+++ index.js
@@ -7,35 +7,37 @@ import smith from './static'
 import smithWatch from 'metalsmith-watch'
 
 const port = process.env.PORT || 3000
-
 const app = express()
+const env = app.get('env')
 
-const webpackConfig = require('./webpack.config')
-const compiler = webpack(webpackConfig)
+if (env === 'development') {
+  const webpackConfig = require('./webpack.config')
+  const compiler = webpack(webpackConfig)
 
-app.use(webpackDevMiddleware(compiler, webpackConfig.devServer))
-app.use(webpackHotMiddleware(compiler))
+  app.use(webpackDevMiddleware(compiler, webpackConfig.devServer))
+  app.use(webpackHotMiddleware(compiler))
+
+  smith.use(smithWatch({
+    'src/site/**/*': true,
+    livereload: true,
+  }))
+
+  smith.build(err => {
+    if (err) {
+      console.error('Metalsmith error', err)
+      process.exit(1)
+    }
+    console.log('Metalsmith built')
+  })
+}
 
 app.use(express.static('static/build'))
 
-smith.use(smithWatch({
-  'src/site/**/*': true,
-  livereload: true,
-}))
-
-const index = fs.readFileSync('./index.html', {encoding: 'utf-8'})
+const index = fs.readFileSync(`./index-${env}.html`, {encoding: 'utf-8'})
 app.get('*', (req, res) => {
   res.send(index)
 })
 
-smith.build(err => {
-  if (err) {
-    return console.error('Metalsmith error', err)
-  }
-
-  app.listen(port, () => {
-    console.log(`Listening on ${port}`)
-  })
-})
+app.listen(port, () => console.log(`Listening on ${port}`))
 
 export default app
