37a0a000da42e127acf60da7ccfa7ff153b05ca2
Jeremy Wells <jemmyw@gmail.com>
2016-06-03 23:06:44 +1200

    feat(StaticSite): Manually set up browser-sync due to broken metalsmith watch

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import fs from 'fs'
+import path from 'path'
 import express from 'express'
 import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 import smith from './static'
 import smithWatch from 'metalsmith-watch'
+import browserSync from 'browser-sync'
 
 const port = process.env.PORT || 3000
 const app = express()
@@ -13,15 +15,34 @@ const env = app.get('env')
 if (env === 'development') {
   const webpackConfig = require('./webpack.config')
   const compiler = webpack(webpackConfig)
+  const hot = webpackHotMiddleware(compiler)
+  const sync = browserSync.create()
 
   app.use(webpackDevMiddleware(compiler, webpackConfig.devServer))
-  app.use(webpackHotMiddleware(compiler))
+  app.use(hot)
 
-  smith.use(smithWatch({
-    'src/site/**/*': true,
-    'src/layouts/*': '**/*',
-    livereload: true,
+  sync.init({
+    proxy: `localhost:${port}`,
+    notify: false,
+  }, () => {
+    console.log('BrowserSync ready')
+  })
+
+  sync.watch('static/build/*.html').on('change', sync.reload)
+  sync.watch('static/build/**/*.css', {
+    awaitWriteFinish: {
+      stabilityThreshold: 1000,
+    }}).on('change', () => sync.reload('*.css'))
+  sync.watch('static/src/**/*').on('change', () => smith.build(err => {
+    if (err) { return console.log('build error') }
+    console.log('rebuilt')
   }))
+
+  // smith.use(smithWatch({
+  //   'src/site/**/*': true,
+  //   'src/layouts/*': '**/*',
+  //   livereload: true,
+  // }))
 
   smith.build(err => {
     if (err) {
